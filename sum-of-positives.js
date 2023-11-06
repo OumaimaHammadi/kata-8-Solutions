5cb3a33310bbc329b0a3ef08b6867cccd408f1dd
oumaima <hammadiioumaima@gmail.com>
2023-11-07 00:21:30 +0100

    sum of positives

--- sum-of-positives.js
+++ sum-of-positives.js
@@ -1,12 +1,30 @@
 function positive_Sum(arr){
-    if(arr[i]>0 ){
-        return 'positive'
-    }
-    else{
-        return 'negative'
+/*1ere Methode */
 
-    }
+  /*   let sum=0
+    for(let i=0;i<arr.length;i++ ){
+        if( arr[i]>0 ){
+            console.log("positive")
+           sum+=arr[i]
+        }
+        // else{
+        // console.log('negative')
+    
+        // }
+
+    } 
+    
+    
+        return sum
+
+    */
+
+
+    /*2éme Methode */
+    return arr.filter((x)=> x > 0).reduce((acc,current)=>acc + current,0)
+
+ 
 }
 
 
-console.log(positive_Sum([1,2,3,3]))
+console.log(positive_Sum([1,2,-3,3]))

dbd4887d3cb2adac748568ab8dd7ffd1d0ab9938
oumaima <hammadiioumaima@gmail.com>
2023-11-09 03:02:11 +0100

    sum without The-Highest-And-The-Lowest-Number

---
+++ sum-without-H_L.js
@@ -0,0 +1,26 @@
+//Sum-Without-The-Highest-And-The-Lowest-Number
+function sum_Array(array){
+   if(array == null) return 0
+  /*  return array
+   .sort((a,b) => a-b)
+   .slice(1,-1)
+   .reduce((acc,current)=> acc + current,0) */
+
+
+   return array
+   .filter((x)=>x!==Math.min(...array) && x!== Math.max(...array))
+   .reduce((acc,current) => acc + current ,0)
+
+
+
+
+
+   // let max_value=Math.max(...array)
+   // let min_value = Math.min(...array)
+   // let filterArray = array.filter((x) => x!==max_value && x!==min_value)
+   // let newArray = filterArray.reduce((acc,current) => acc + current ,0)
+   // console.log(newArray)
+}
+console.log(sum_Array([1,10,8,10,7,3])
+)
+

5aa6150d488da518c387dbb2c478c6afb635b2ee
oumaima <hammadiioumaima@gmail.com>
2023-11-09 06:13:46 +0100

    Salutation.js file

---
+++ Salutation.js
@@ -0,0 +1,19 @@
+function Salutation(name){
+        return name !=="Oumaima" ? "Hello," + name + "!" : "Hello ," + name + "  my Love !"  
+
+
+
+    // if(name === "Oumaima"){
+    //     return "Hello ," + name + "  my Love !"
+
+
+    // }
+    // else{
+    //     return "Hello," + name + "!"
+
+    // }
+
+}
+
+
+console.log(Salutation("Oumaima"))

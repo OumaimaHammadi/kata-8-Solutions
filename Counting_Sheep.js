6807cd91955f40c96e869102ec2016897dde74f1
oumaima <hammadiioumaima@gmail.com>
2023-11-09 05:21:36 +0100

    push Counting_Sheep

---
+++ Counting_Sheep.js
@@ -0,0 +1,17 @@
+function Counting_Sheep(arrayOfSheep){
+    let count=0
+    arrayOfSheep.map((x)=>{
+       if(x ===true) return count++
+   
+    })
+   
+      return count
+
+
+
+
+
+}
+
+
+console.log(Counting_Sheep([false,true,true]))

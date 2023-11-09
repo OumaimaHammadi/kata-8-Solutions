916a80b80a66c57f1b163b2e1067086500b21c45
oumaima <hammadiioumaima@gmail.com>
2023-11-09 21:17:04 +0100

    push needle

---
+++ Needle In The HayStack.js
@@ -0,0 +1,28 @@
+function findNeedle(array){
+    /*Method 2 */
+    return "needle is finded  " + array.indexOf('needle')
+
+
+
+
+ 
+
+    /*Method 1 */
+
+//      for(let i=0;i<array.length;i++ ){
+
+
+
+//         if( array[i]=="needle" ){
+//             return "needle is finded  "+ i
+
+
+//         }
+
+      
+// }
+
+}
+
+
+console.log(findNeedle(['hay','junk','hay','hay','moreJunk','needle']))
